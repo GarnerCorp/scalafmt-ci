9d1eebddbe3077c6b61ad8e82e65e02b17576a1d
Zimo Li <7163127+lzm0@users.noreply.github.com>
2024-06-24 13:17:54 -0400

    Infer scalafmt version from `.scalafmt.conf` instead of using arg (#5)
    * Infer scalafmt version from `.scalafmt.conf` instead of using arg
    
    * Add GitHub step summary messages for failed and successful runs

--- run-scalafmt.sh
+++ run-scalafmt.sh
@@ -3,18 +3,11 @@ output=$(mktemp)
 warnings=$(mktemp)
 errors=$(mktemp)
 
-enforce_scala_format_version() {
-  if [ -e .scalafmt.conf ]; then
-    perl -pi -e 'next unless s/^\s*(version)=.*/$1=$ENV{scalafmt_version}/' .scalafmt.conf
-  fi
-}
-
-enforce_scala_format_version
 scalafmt --non-interactive $args > "$output" 2> "$warnings"
 result=$?
 cat "$output"
 cat "$warnings" >&2
-[ $result -eq 0 ] && exit
+[ $result -eq 0 ] && echo "All files well-formatted ✨" >> "$GITHUB_STEP_SUMMARY" && exit
 
 report() {
     file="$1"
